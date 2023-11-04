a254e9244daf3112186bf117d0b2798cd6e2af9d
Rafael Scarduelli <rafael.scarduelli@loadsmart.com>
2023-11-04 19:33:11 -0300

    quiz1

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,11 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(amount: u32) -> u32 {
+    let price_ea = if amount > 40 { 1 } else { 2 };
+    amount * price_ea
+}
 
 // Don't modify this function!
 #[test]
