e11a94010400288eecf756a1aedb49bc8c66805f
Rafael Scarduelli <rafael.scarduelli@loadsmart.com>
2023-11-15 18:09:16 -0300

    from_into

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -40,10 +40,30 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of
 // Person Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
-
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        let mut s = s.split(',');
+
+        let name = match s.next() {
+            Some(name) => name,
+            None => return Person::default(),
+        };
+
+        let age = match s.next() {
+            Some(age) => age,
+            None => return Person::default(),
+        };
+
+        if let Ok(age) = age.parse::<usize>() {
+            if !name.is_empty() {
+                return Person {
+                    name: String::from(name),
+                    age,
+                };
+            }
+        }
+
+        Person::default()
     }
 }
 
