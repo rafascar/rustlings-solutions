2aa795f20d63b92c3dffe7bed61bfcf13a22819b
Rafael Scarduelli <rafael.scarduelli@loadsmart.com>
2023-11-15 22:32:01 -0300

    try_from_into

--- exercises/conversions/try_from_into.rs
+++ exercises/conversions/try_from_into.rs
@@ -9,7 +9,10 @@
 // Execute `rustlings hint try_from_into` or use the `hint` watch subcommand for
 // a hint.
 
-use std::convert::{TryFrom, TryInto};
+use std::{
+    convert::{TryFrom, TryInto},
+    num::TryFromIntError,
+};
 
 #[derive(Debug, PartialEq)]
 struct Color {
@@ -27,8 +30,6 @@ enum IntoColorError {
     IntConversion,
 }
 
-// I AM NOT DONE
-
 // Your task is to complete this implementation and return an Ok result of inner
 // type Color. You need to create an implementation for a tuple of three
 // integers, an array of three integers, and a slice of integers.
@@ -37,10 +38,21 @@ enum IntoColorError {
 // time, but the slice implementation needs to check the slice length! Also note
 // that correct RGB color values must be integers in the 0..=255 range.
 
+impl From<TryFromIntError> for IntoColorError {
+    fn from(value: TryFromIntError) -> Self {
+        IntoColorError::IntConversion
+    }
+}
+
 // Tuple implementation
 impl TryFrom<(i16, i16, i16)> for Color {
     type Error = IntoColorError;
     fn try_from(tuple: (i16, i16, i16)) -> Result<Self, Self::Error> {
+        Ok(Color {
+            red: u8::try_from(tuple.0)?,
+            green: u8::try_from(tuple.1)?,
+            blue: u8::try_from(tuple.2)?,
+        })
     }
 }
 
@@ -48,6 +60,11 @@ impl TryFrom<(i16, i16, i16)> for Color {
 impl TryFrom<[i16; 3]> for Color {
     type Error = IntoColorError;
     fn try_from(arr: [i16; 3]) -> Result<Self, Self::Error> {
+        Ok(Color {
+            red: u8::try_from(arr[0])?,
+            green: u8::try_from(arr[1])?,
+            blue: u8::try_from(arr[2])?,
+        })
     }
 }
 
@@ -55,6 +72,15 @@ impl TryFrom<[i16; 3]> for Color {
 impl TryFrom<&[i16]> for Color {
     type Error = IntoColorError;
     fn try_from(slice: &[i16]) -> Result<Self, Self::Error> {
+        if slice.len() != 3 {
+            return Err(IntoColorError::BadLen);
+        }
+
+        Ok(Color {
+            red: u8::try_from(slice[0])?,
+            green: u8::try_from(slice[1])?,
+            blue: u8::try_from(slice[2])?,
+        })
     }
 }
 
